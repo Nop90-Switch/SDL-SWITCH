462d8ae0c6039b00a29d5e0a132e60e161ac9e60
nop90 <nop90nop90@gmail.com>
2018-02-24 20:35:03 +0100

    Initial commit
    Ported video drive and input handling - not tested yet

---
+++ SDL-1.2.15/src/video/switch/SDL_switchevents.c
@@ -0,0 +1,142 @@
+/*
+    SDL - Simple DirectMedia Layer
+    Copyright (C) 1997-2012 Sam Lantinga
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+    Sam Lantinga
+    slouken@libsdl.org
+*/
+#include "SDL_config.h"
+
+/* Being a null driver, there's no event stream. We just define stubs for
+   most of the API. */
+
+#include "switch.h"
+#include "../../events/SDL_sysevents.h"
+#include "../../events/SDL_events_c.h"
+
+#include "SDL_switchvideo.h"
+#include "SDL_switchevents_c.h"
+
+static SDLKey keymap[SWITCH_NUMKEYS];
+char keymem[SWITCH_NUMKEYS];
+
+void SWITCH_PumpEvents(_THIS)
+{
+	if(!appletMainLoop())
+	{
+		SDL_Event sdlevent;
+		sdlevent.type = SDL_QUIT;
+		SDL_PushEvent(&sdlevent);
+	} 
+	
+	hidScanInput();
+
+	int i;
+	SDL_keysym keysym;
+	keysym.mod = KMOD_NONE;
+
+	for (i = 0; i < SWITCH_NUMKEYS; i++) {
+		keysym.scancode = i;
+		keysym.sym = keymap[i];
+
+		if (hidKeysHeld(CONTROLLER_P1_AUTO) & (1 << i) && !keymem[i]) {
+			keymem[i] = 1;
+
+			SDL_PrivateKeyboard (SDL_PRESSED, &keysym);
+		}
+
+		if (!(hidKeysHeld(CONTROLLER_P1_AUTO) & (1 << i)) && keymem[i]) {
+			keymem[i] = 0;
+
+			SDL_PrivateKeyboard (SDL_RELEASED, &keysym);
+		}
+	}
+/*
+	if (hidKeysHeld(CONTROLLER_P1_AUTO) & KEY_TOUCH) {
+		touchPosition touch;
+
+		hidTouchRead (&touch);
+		
+// TO DO: handle fit screen on x and y.Y and Y to be considered separately
+		
+		if(this->hidden->screens&SDL_TOPSCR && this->hidden->screens&SDL_BOTTOMSCR) {
+			if (touch.px != 0 || touch.py != 0) {
+				SDL_PrivateMouseMotion (0, 0, 
+					touch.px  + (this->hidden->w1 - 320)/2, 
+					this->hidden->y2 + touch.py + (this->hidden->h2 - 240)/2);
+				if (!SDL_GetMouseState (NULL, NULL))
+					SDL_PrivateMouseButton (SDL_PRESSED, 1, 0, 0);
+			}
+		} else {
+			if (touch.px != 0 || touch.py != 0) {
+				SDL_PrivateMouseMotion (0, 0, (touch.px * this->hidden->w1) / 320, (touch.py * this->hidden->h1) / 240);
+				if (!SDL_GetMouseState (NULL, NULL))
+					SDL_PrivateMouseButton (SDL_PRESSED, 1, 0, 0);
+			}
+		}
+	} else {
+		if (SDL_GetMouseState (NULL, NULL))
+			SDL_PrivateMouseButton (SDL_RELEASED, 1, 0, 0);
+	}
+*/
+}
+
+void SWITCH_InitOSKeymap(_THIS)
+{
+	SDL_memset(keymem,1,SWITCH_NUMKEYS);
+	keymap[0]=SDLK_a; //KEY_A
+	keymap[1]=SDLK_b; // KEY_B
+	keymap[2]=SDLK_x; //KEY_X 
+	keymap[3]=SDLK_y; //KEY_Y
+	keymap[4]=SDLK_LSHIFT;  //left stick button 
+	keymap[5]=SDLK_RSHIFT;  //right stick button
+	keymap[6]=SDLK_l; //KEY_L
+	keymap[7]=SDLK_r; //KEY_R
+	keymap[8]=SDLK_j; //KEY_ZL
+	keymap[9]=SDLK_k; //KEY_ZR
+	keymap[10]=SDLK_MINUS; //KEY_MINUS 
+	keymap[11]=SDLK_PLUS; //KEY_PLUS
+	keymap[12]=SDLK_LEFT; //KEY_LEFT
+	keymap[13]=SDLK_UP; // KEY_UP
+	keymap[14]=SDLK_RIGHT; //KEY_RIGHT
+	keymap[15]=SDLK_DOWN; //KEY_DOWN
+	keymap[16]=SDLK_UNKNOWN; 
+	keymap[17]=SDLK_UNKNOWN; 
+	keymap[18]=SDLK_UNKNOWN; 
+	keymap[19]=SDLK_UNKNOWN; 
+	keymap[20]=SDLK_UNKNOWN; 
+	keymap[21]=SDLK_UNKNOWN; 
+	keymap[22]=SDLK_UNKNOWN; 
+	keymap[23]=SDLK_UNKNOWN; 
+	keymap[24]=SDLK_UNKNOWN; 
+	keymap[25]=SDLK_UNKNOWN; 
+	keymap[26]=SDLK_UNKNOWN; 
+	keymap[27]=SDLK_UNKNOWN; 
+	keymap[28]=SDLK_UNKNOWN; 
+	keymap[29]=SDLK_UNKNOWN; 
+	keymap[30]=SDLK_UNKNOWN; 
+	keymap[31]=SDLK_UNKNOWN; 
+
+// init the key state
+	int i;
+	hidScanInput();
+	for (i = 0; i < SWITCH_NUMKEYS; i++)
+		keymem[i] = (hidKeysHeld(CONTROLLER_P1_AUTO) & (1 << i))?1:0;
+}
+
+/* end of SDL_nullevents.c ... */
+
---
+++ SDL-1.2.15/src/video/switch/SDL_switchvideo.c
@@ -0,0 +1,252 @@
+/*
+    SDL - Simple DirectMedia Layer
+    Copyright (C) 1997-2012 Sam Lantinga
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+    Sam Lantinga
+    slouken@libsdl.org
+*/
+#include "SDL_config.h"
+#include <switch.h>
+
+/* SWITCH SDL video driver implementation; 
+ */
+
+#include "SDL_video.h"
+#include "SDL_mouse.h"
+#include "../SDL_sysvideo.h"
+#include "../SDL_pixels_c.h"
+#include "../../events/SDL_events_c.h"
+
+#include "SDL_switchvideo.h"
+#include "SDL_switchevents_c.h"
+#include "SDL_switchmouse_c.h"
+
+#define SWITCHVID_DRIVER_NAME "switch"
+
+/* Initialization/Query functions */
+static int SWITCH_VideoInit(_THIS, SDL_PixelFormat *vformat);
+static SDL_Rect **SWITCH_ListModes(_THIS, SDL_PixelFormat *format, Uint32 flags);
+static SDL_Surface *SWITCH_SetVideoMode(_THIS, SDL_Surface *current, int width, int height, int bpp, Uint32 flags);
+static int SWITCH_SetColors(_THIS, int firstcolor, int ncolors, SDL_Color *colors);
+static void SWITCH_VideoQuit(_THIS);
+
+/* Hardware surface functions */
+static int SWITCH_AllocHWSurface(_THIS, SDL_Surface *surface);
+static int SWITCH_LockHWSurface(_THIS, SDL_Surface *surface);
+static void SWITCH_UnlockHWSurface(_THIS, SDL_Surface *surface);
+static void SWITCH_FreeHWSurface(_THIS, SDL_Surface *surface);
+static int SWITCH_FlipHWSurface (_THIS, SDL_Surface *surface);
+
+/* etc. */
+static void SWITCH_UpdateRects(_THIS, int numrects, SDL_Rect *rects);
+
+/* SWITCH driver bootstrap functions */
+
+static int SWITCH_Available(void)
+{
+	return(1);
+/*	
+	const char *envr = SDL_getenv("SDL_VIDEODRIVER");
+	if ((envr) && (SDL_strcmp(envr, SWITCHVID_DRIVER_NAME) == 0)) {
+		return(1);
+	}
+
+	return(0);
+*/
+}
+
+static void SWITCH_DeleteDevice(SDL_VideoDevice *device)
+{
+	SDL_free(device->hidden);
+	SDL_free(device);
+}
+
+static SDL_VideoDevice *SWITCH_CreateDevice(int devindex)
+{
+	SDL_VideoDevice *device;
+
+	/* Initialize all variables that we clean on shutdown */
+	device = (SDL_VideoDevice *)SDL_malloc(sizeof(SDL_VideoDevice));
+	if ( device ) {
+		SDL_memset(device, 0, (sizeof *device));
+		device->hidden = (struct SDL_PrivateVideoData *)
+				SDL_malloc((sizeof *device->hidden));
+	}
+	if ( (device == NULL) || (device->hidden == NULL) ) {
+		SDL_OutOfMemory();
+		if ( device ) {
+			SDL_free(device);
+		}
+		return(0);
+	}
+	SDL_memset(device->hidden, 0, (sizeof *device->hidden));
+
+	/* Set the function pointers */
+	device->VideoInit = SWITCH_VideoInit;
+	device->ListModes = SWITCH_ListModes;
+	device->SetVideoMode = SWITCH_SetVideoMode;
+	device->CreateYUVOverlay = NULL;
+	device->SetColors = SWITCH_SetColors;
+	device->UpdateRects = SWITCH_UpdateRects;
+	device->VideoQuit = SWITCH_VideoQuit;
+	device->AllocHWSurface = SWITCH_AllocHWSurface;
+	device->CheckHWBlit = NULL;
+	device->FillHWRect = NULL;
+	device->SetHWColorKey = NULL;
+	device->SetHWAlpha = NULL;
+	device->LockHWSurface = SWITCH_LockHWSurface;
+	device->UnlockHWSurface = SWITCH_UnlockHWSurface;
+	device->FlipHWSurface = SWITCH_FlipHWSurface;
+	device->FreeHWSurface = SWITCH_FreeHWSurface;
+	device->SetCaption = NULL;
+	device->SetIcon = NULL;
+	device->IconifyWindow = NULL;
+	device->GrabInput = NULL;
+	device->GetWMInfo = NULL;
+	device->InitOSKeymap = SWITCH_InitOSKeymap;
+	device->PumpEvents = SWITCH_PumpEvents;
+
+	device->free = SWITCH_DeleteDevice;
+
+	return device;
+}
+
+VideoBootStrap SWITCH_bootstrap = {
+	SWITCHVID_DRIVER_NAME, "SDL Switch video driver",
+	SWITCH_Available, SWITCH_CreateDevice
+};
+
+
+int SWITCH_VideoInit(_THIS, SDL_PixelFormat *vformat)
+{
+	gfxInitDefault();
+	vformat->BitsPerPixel = 32;
+	vformat->BytesPerPixel = 4;
+
+	/* We're done! */
+	return(0);
+}
+
+SDL_Rect **SWITCH_ListModes(_THIS, SDL_PixelFormat *format, Uint32 flags)
+{
+   	 return (SDL_Rect **) -1;
+}
+
+SDL_Surface *SWITCH_SetVideoMode(_THIS, SDL_Surface *current,
+				int width, int height, int bpp, Uint32 flags)
+{
+	if ( this->hidden->buffer ) {
+		SDL_free( this->hidden->buffer );
+	}
+
+	this->hidden->buffer = SDL_malloc(width * height * (bpp / 8));
+	if ( ! this->hidden->buffer ) {
+		SDL_SetError("Couldn't allocate buffer for requested mode");
+		return(NULL);
+	}
+
+/* 	printf("Setting mode %dx%d\n", width, height); */
+
+	SDL_memset(this->hidden->buffer, 0, width * height * (bpp / 8));
+
+	/* Allocate the new pixel format for the screen */
+	if ( ! SDL_ReallocFormat(current, bpp, 0, 0, 0, 0) ) {
+		SDL_free(this->hidden->buffer);
+		this->hidden->buffer = NULL;
+		SDL_SetError("Couldn't allocate new pixel format for requested mode");
+		return(NULL);
+	}
+
+	/* Set up the new mode framebuffer */
+	current->flags = flags & SDL_FULLSCREEN;
+	this->hidden->w = current->w = width;
+	this->hidden->h = current->h = height;
+	current->pitch = current->w * (bpp / 8);
+	current->pixels = this->hidden->buffer;
+
+	/* We're done */
+	return(current);
+}
+
+/* We don't actually allow hardware surfaces other than the main one */
+static int SWITCH_AllocHWSurface(_THIS, SDL_Surface *surface)
+{
+	return(-1);
+}
+static void SWITCH_FreeHWSurface(_THIS, SDL_Surface *surface)
+{
+	return;
+}
+
+/* We need to wait for vertical retrace on page flipped displays */
+static int SWITCH_LockHWSurface(_THIS, SDL_Surface *surface)
+{
+	return(0);
+}
+
+static void SWITCH_UnlockHWSurface(_THIS, SDL_Surface *surface)
+{
+	return;
+}
+
+static void SWITCH_UpdateRects(_THIS, int numrects, SDL_Rect *rects)
+{
+	/* do nothing. */
+}
+
+int SWITCH_SetColors(_THIS, int firstcolor, int ncolors, SDL_Color *colors)
+{
+	/* do nothing of note. */
+	return(1);
+}
+
+/* Note:  If we are terminated, this could be called in the middle of
+   another SDL video routine -- notably UpdateRects.
+*/
+void SWITCH_VideoQuit(_THIS)
+{
+	if (this->screen->pixels != NULL)
+	{
+		SDL_free(this->screen->pixels);
+		this->screen->pixels = NULL;
+	}
+	gfxExit();
+}
+
+static int SWITCH_FlipHWSurface (_THIS, SDL_Surface *surface) {
+	u32 width, height;
+	u32 pos_src, pos_dst;
+	u32* framebuf = (u32*) gfxGetFramebuffer((u32*)&width, (u32*)&height);
+	u8* videobuf = (u8*) this->screen->pixels;
+	//Each pixel is 4-bytes due to RGBA8888.
+	u32 x, y;
+	for (y=0; y<(height< this->screen->h)?height: this->screen->h; y++)//Access the buffer linearly.
+	{
+		for (x=0; x<(width < this->screen->w)?width: this->screen->w; x++)
+		{
+			pos_dst = y * width + x;
+			pos_src = (y * this->screen->w + x)*4;
+			framebuf[pos_dst] = RGBA8(videobuf[pos_src+0], videobuf[pos_src+1], videobuf[pos_src+2], videobuf[pos_src+4]);
+		}
+	}
+
+	gfxFlushBuffers();
+	gfxSwapBuffers();
+	gfxWaitForVsync();
+
+	return (0);
+}
